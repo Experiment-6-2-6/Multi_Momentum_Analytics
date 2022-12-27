8acefebb963991d21050e9813e66dd5c7bf5bb71
Kristof <41172564+Experiment-6-2-6@users.noreply.github.com>
2022-12-27 22:50:20 +0000

    Rename MM_Workflow to Analytics_Workflow Complete re-work of the code Only plot left to be done later

--- double_momentum_r_script.R
+++ double_momentum_r_script.R
@@ -1,18 +1,18 @@
-# Packages Installation --------------------------------------------------------
+# Packages ---------------------------------------------------------------------
 install.packages("quantmod")
 install.packages("tidyverse")
 install.packages("RPostgres")
 install.packages("knitr")
 
-# Libraries -------------------------------------------------------------
+# Libraries --------------------------------------------------------------------
 library(quantmod)
 library(tidyverse)
 library(dplyr)
 library(RPostgres)
-library(ggplot2)
 library(knitr)
+library(ggplot2)
 
-# Establishing DB connection ---------------------------------------------------
+# Postgres DB connection -------------------------------------------------------
 my_postgr <- dbConnect(
   RPostgres::Postgres(),
   host = Sys.getenv("sql_host"),
@@ -23,106 +23,126 @@ my_postgr <- dbConnect(
   sslmode = "require"
 )
 
-# Getting Raw Data -------------------------------------------------------------
+# Getting the Data -------------------------------------------------------------
 start_date <- Sys.Date() - (3 * 365)
 end_date <- Sys.Date()
 etf_qts_env <- new.env()
 
 etf_query <- dbGetQuery(my_postgr, "SELECT * FROM etf_info;")
-# Getting the proper set of ETFs
-assets_names <- as.vector(etf_query[["ticker_usd"]]) # ticker_gbp is available
-yahoo_symbols <- as.vector(etf_query[["symbol_usd"]])# in my DB but there is no 
-# real difference in performance they are only quoted in different currencies
-getSymbols(yahoo_symbols,  from = start_date, to = Sys.Date(), env = etfs_quotes)
-daily_closes <- do.call(merge, eapply(etfs_quotes, Ad))
-from_web <- paste0("https://stooq.com/q/d/l/?s=xauusd&d1=", 
-                  format(start_date, "%Y%m%d"),
-                  "&d2=", 
-                  format(end_date, "%Y%m%d"),
-                  "&i=d")
-gold_qts <- read_csv(from_web)
-gold_qts <- xts(gold_tmp$Close, order.by = gold_tmp$Date)
-daily_closes <- merge(daily_closes, gold_qts)
-daily_closes <- na.omit(daily_closes)
-assets_names <- c(assets_names,"IGLN")
-names(daily_closes) <- assets_names
-temp_df$FTSE <- NULL # FTSE index no longer needed (dates synch done)
+# etfs_tickers <- as.vector(etf_query[["ticker_usd"]])
+yahoo_symbols <- as.vector(etf_query[["symbol_usd"]])
+# etfs_names <- as.vector(etf_query[["full_name"]])
 
-# Writing the DB (quotes) ------------------------------------------------------
-# Transforming quotes into PostgreSQL format before sending to DB
-df_for_sql <- as.data.frame(temp_df)
+# daily quotes
+getSymbols(yahoo_symbols, 
+           from = start_date, 
+           to = end_date, 
+           periodicity = "daily",
+           env = etf_qts_env)
+daily_closes <- do.call(merge, eapply(etf_qts_env, Ad))
+daily_closes <- na.omit(daily_closes)
+names(daily_closes) <- gsub(".L.Adjusted", "", names(daily_closes))
+
+# weekly quotes
+getSymbols(yahoo_symbols, 
+           from = start_date, 
+           to = end_date, 
+           periodicity = "weekly",
+           env = etf_qts_env)
+weekly_closes <- do.call(merge, eapply(etf_qts_env, Ad))
+weekly_closes <- na.omit(weekly_closes)
+names(weekly_closes) <- gsub(".L.Adjusted", "", names(weekly_closes))
+
+# Writing into DB (quotes) -----------------------------------------------------
+# Have to transform into PostgreSQL format before sending to DB
+df_for_sql <- as.data.frame(daily_closes)
 df_for_sql <- cbind(date = as.Date(rownames(df_for_sql)), df_for_sql)
-if (dbExistsTable(my_postgr, "quotes_usd")) {
-  dbGetQuery(my_postgr, "DROP TABLE quotes_usd;")
-} else {
-  dbWriteTable(my_postgr, "quotes_usd", df_for_sql, row.names = FALSE)
+if (dbExistsTable(my_postgr, "quotes_daily")) {
+  dbRemoveTable(my_postgr, "quotes_daily")
 }
+dbWriteTable(my_postgr, "quotes_daily", df_for_sql, row.names = FALSE)
+
+df_for_sql <- as.data.frame(weekly_closes)
+df_for_sql <- cbind(date = as.Date(rownames(df_for_sql)), df_for_sql)
+if (dbExistsTable(my_postgr, "quotes_weekly")){
+  dbRemoveTable(my_postgr, "quotes_weekly")
+}
+dbWriteTable(my_postgr, "quotes_weekly", df_for_sql, row.names = FALSE)
 
 # Absolute Momentum Check ------------------------------------------------------
-# I will need it to validate the leaderboard candidates
-price_above_ema <- rep(TRUE, ncol(temp_df))
-names(price_above_ema) <- assets_names
-if (nrow(temp_df[1]) < 30 ){
-  data_is_too_short <- TRUE
-} else {
-  data_is_too_short <- FALSE
-  for (ticker in assets_names){
-    weekly_data <- Cl(to.weekly(temp_df[[ticker]]))
-    ema_30w <- EMA(weekly_data, n = 30)
-    price_above_ema[[ticker]] <- if (ema_30w < last(weekly_data)) TRUE else FALSE
-  }
+positive_momentum <- rep(TRUE, ncol(weekly_closes))
+names(positive_momentum) <- names(weekly_closes)
+for (ticker in names(weekly_closes)){
+  ema30w <- EMA(weekly_closes[,ticker], n = 30)
+  if (last(ema30w) < last(weekly_closes[,ticker])) {
+    positive_momentum[[ticker]] <- TRUE
+  } else {
+      positive_momentum[[ticker]] <- FALSE
+    }
 }
 
 # Relative Momentum Calculation ------------------------------------------------
-# I have chosen the simple Rate Of Change calculation
-# because I want it to be between -100% and +100% and not -∞ to +∞
-# ROC "descrete" = simple (aritmethic) return,
-# ROC "continous" = logarithmic (geometric) method
-daily_1MM <- map(temp_df, ROC, n = 21, type = "discrete") # <<TO DO>>
+daily_1ym <- ROC(daily_closes, n = 252, type = "discrete") %>%  
+              na.omit() %>% "*"(100) %>% round(2)
+weekly_1ym <- ROC(weekly_closes, n = 50, type = "discrete") %>%  
+                na.omit() %>% "*"(100) %>% round(2)
 
+# Writing into DB (momentum) ---------------------------------------------------
+df_for_sql <- as.data.frame(daily_1ym)
+df_for_sql <- cbind(date = as.Date(rownames(df_for_sql)), df_for_sql)
+if (dbExistsTable(my_postgr, "momentum_d")) {
+  dbRemoveTable(my_postgr, "momentum_d")
+}
+dbWriteTable(my_postgr, "momentum_d", df_for_sql, row.names = FALSE)
 
-daily_1YM_merged <- ROC(temp_df[[1]], n = 252, type = "discrete")
-weekly_1YM_merged <- to.weekly(temp_df[[1]], indexAt = "endof") %>% 
-  Cl() %>% 
-  ROC(., n = 50, type = "discrete") %>% 
-  "*"(100) %>% 
-  round(., 2)
-for (i in seq_along(assets_names)){
-  daily_1YM <- ROC(temp_df[[i]], 252)
-  
-  weekly_1YM <- to.weekly(temp_df[,i], indexAt = "endof") %>% 
-    Cl() %>% 
-    ROC(., n = 50, type = "discrete") %>% 
-    "*"(100) %>% 
-    round(., 2)
-  
-  daily_1YM_merged <- merge(daily_1YM_merged, daily_1YM)
-  weekly_1MM_merged <- merge(weekly_1MM_merged, weekly_1MM)
-  weekly_1YM_merged <- merge(weekly_1YM_merged, weekly_1YM)
+df_for_sql <- as.data.frame(weekly_1ym)
+df_for_sql <- cbind(date = as.Date(rownames(df_for_sql)), df_for_sql)
+if (dbExistsTable(my_postgr, "momentum_w")) {
+  dbRemoveTable(my_postgr, "momentum_w")
+}
+dbWriteTable(my_postgr, "momentum_w", df_for_sql, row.names = FALSE)
+
+# Connection Off ---------------------------------------------------------------
+dbDisconnect(my_postgr)
+
+# Leaderboard Table ------------------------------------------------------------
+temp_df <- as_tibble(weekly_1ym)
+temp_df <- temp_df[,order(temp_df[nrow(temp_df),],decreasing = TRUE)]
+temp_df <- tail(temp_df, 1)
+
+descriptions <- NULL
+for (ticker in names(temp_df)) {
+  description <- etf_query$full_name[etf_query$ticker_usd == ticker]
+  descriptions <- append(descriptions, description)
 }
 
-daily_1YM_merged <- na.omit(daily_1YM_merged)
-weekly_1YM_merged <- na.omit(weekly_1YM_merged)
+gbx_tickers <- NULL
+for (ticker in names(temp_df)) {
+  gbx_ticker <- etf_query$ticker_gbp[etf_query$ticker_usd == ticker]
+  gbx_tickers <- append(gbx_tickers, gbx_ticker)
+}
 
-names(daily_1YM_merged) <- assets_names
-names(weekly_1YM_merged) <- assets_names
 
-daily_1YM_merged <- as.data.frame(daily_1YM_merged)
-weekly_1YM_merged <- as.data.frame(weekly_1YM_merged)
+leaderboard_data <- tibble(descriptions,
+                           names(temp_df),
+                           gbx_tickers,
+                           positive_momentum,
+                           t(temp_df))
 
-# Writting the Calculations to DB ----------------------------------------------
-df_ym_d_for_sql <- data.frame(Date = as.Date(rownames(daily_1YM_merged)), daily_1YM_merged)
-df_ym_w_for_sql <- data.frame(Date = as.Date(rownames(weekly_1YM_merged)), weekly_1YM_merged)
+names(leaderboard_data) <- c("ETF Full Name / Description",
+                             "USD", "GBP", "Positive Mm.", "1Y Mm. %")
 
-df_ym_w_for_sql <- tail(df_ym_w_for_sql, 50)
+knitr::kable(leaderboard_data, 
+             "pipe", 
+             align = c("l", "c", "c", "c", "r"),
+             caption = "ETFs Leaderboard (In Descending Order)")
 
-if (dbExistsTable(my_postgr, "ym_daily")) {
-  dbGetQuery(my_postgr, "DROP TABLE ym_daily;")
-} else dbWriteTable(my_postgr, "ym_daily", df_ym_d_for_sql, row.names = FALSE)
+# Plot -------------------------------------------------------------------------
+plot_data <- tibble(date = df_for_sql$date, 
+                    select(df_for_sql, 
+                           leaderboard_data[[1, "USD"]], 
+                           leaderboard_data[[2, "USD"]], 
+                           leaderboard_data[[3, "USD"]], 
+                           leaderboard_data[[4, "USD"]], 
+                           leaderboard_data[[5, "USD"]]))
 
-if (dbExistsTable(my_postgr, "ym_weekly")) {
-  dbGetQuery(my_postgr, "DROP TABLE ym_weekly;")
-} else dbWriteTable(my_postgr, "ym_weekly", df_ym_w_for_sql, row.names = FALSE)
-
-# Terminatin the DB my_postgr
-dbDisconnect(my_postgr)
